b55ed749b0add3913c64cef7600b6f2d3adf1243
Marvin Binder <43495737+BinderDyn@users.noreply.github.com>
2020-01-27 22:20:42 +0100

    GUI Add dialog

---
+++ TakeMyTime.WPF/Entries/AddEntry.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TakeMyTime.WPF.Entries
+{
+    /// <summary>
+    /// Interaktionslogik für AddEntry.xaml
+    /// </summary>
+    public partial class AddEntry : Window
+    {
+        public AddEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void b_Toolbar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
